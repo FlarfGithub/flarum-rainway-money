61840617e2644f030fb18d88ce3ddf7f1be3e5d9
FlarfGithub <84773791+FlarfGithub@users.noreply.github.com>
2023-06-02 11:50:41 +0300

    Update AddUserMoneyAttributes.php

--- src/AddUserMoneyAttributes.php
+++ src/AddUserMoneyAttributes.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace AntoineFr\Money;
+namespace FlarfGithub\Money;
 
 use Flarum\Api\Serializer\UserSerializer;
 use Flarum\User\User;
@@ -12,6 +12,19 @@ class AddUserMoneyAttributes
         $canEditMoney = $serializer->getActor()->can('edit_money', $user);
 
         $attributes = [];
+
+        // Get the current date and the last login date
+        $currentDate = date('Y-m-d');
+        $lastLoginDate = $user->last_seen->toDateString();
+
+        // Check if the user has logged in today
+        $hasLoggedToday = ($currentDate === $lastLoginDate);
+
+        // Give the daily currency stipend if the user has logged in today
+        if ($hasLoggedToday) {
+            $currencyAmount = 50; // Adjust this value to your desired stipend amount
+            GiveMoney::giveCurrency($user, $currencyAmount);
+        }
 
         $attributes['money'] = $user->money;
         $attributes['canEditMoney'] = $canEditMoney;
